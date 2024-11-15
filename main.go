d23c12877f78699ef45827c2a401b2ca4e39794f
lee <876794356@qq.com>
2024-11-15 10:11:46 +0800

    update

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"go-extend/ext"
-	"time"
-)
-
 func main() {
-	println("hello")
-	a := ext.Actor_(4, func(a any) {
-		fmt.Printf("%#v\n", a)
-	})
-	a.Launch(func() {
-		panic("a")
-	})
-	a.Launch(func() {
-		panic("b")
-	})
-	time.Sleep(time.Second)
+
 }
