7f0b2ee1cd516ccb47ff5f271cb7c6e67361ed6f
lee <876794356@qq.com>
2024-12-16 11:29:23 +0800

    Added ForEachWhile, MapWhile,  MapWhileTo function

--- ext/promise.go
+++ ext/promise.go
@@ -4,13 +4,12 @@ import (
 	"errors"
 	"sync"
 	"sync/atomic"
-	"time"
 )
 
 const (
-	PromisePending  = 0
-	PromiseComplete = 1
-	PromiseTimeout  = -1
+	PromisePending   = 0
+	PromiseCompleted = 1
+	PromiseCanceled  = -1
 )
 
 type Promise[T any] struct {
@@ -18,9 +17,9 @@ type Promise[T any] struct {
 }
 
 type _PromisePin[T any] struct {
-	waiter  sync.WaitGroup
-	pending atomic.Int32
-	result  T
+	waiter sync.WaitGroup
+	status atomic.Int32
+	result T
 }
 
 type PromiseRes[T any] struct {
@@ -31,71 +30,70 @@ type PromiseRes[T any] struct {
 func (p PromiseRes[T]) IsPending() bool {
 	return p.Status == PromisePending
 }
-func (p PromiseRes[T]) IsTimeout() bool {
-	return p.Status == PromiseTimeout
+func (p PromiseRes[T]) IsCanceled() bool {
+	return p.Status == PromiseCanceled
 }
-func (p PromiseRes[T]) IsComplete() bool {
-	return p.Status == PromiseComplete
+func (p PromiseRes[T]) IsCompleted() bool {
+	return p.Status == PromiseCompleted
 }
 
 func (p PromiseRes[T]) Get() T {
-	if p.IsComplete() {
+	if p.IsCompleted() {
 		return p.result
 	}
 	panic(errors.New("option is none"))
 }
 
 func (p PromiseRes[T]) Get_() T {
-	if p.IsComplete() {
+	if p.IsCompleted() {
 		return p.result
 	}
 	return *new(T)
 }
 
 func (p PromiseRes[T]) GetOr(t T) T {
-	if p.IsComplete() {
+	if p.IsCompleted() {
 		return p.result
 	}
 	return t
 }
 
 func (p PromiseRes[T]) GetElse(fn func() T) T {
-	if p.IsComplete() {
+	if p.IsCompleted() {
 		return p.result
 	}
 	return fn()
 }
 
-// Await 0未完成 1完成 -1超时
 func (p Promise[T]) Await() PromiseRes[T] {
-	if p.pending.Load() == PromisePending {
+	if p.status.Load() == PromisePending {
 		p.waiter.Wait()
 	}
-	return PromiseRes[T]{int8(p.pending.Load()), p.result}
+	return PromiseRes[T]{int8(p.status.Load()), p.result}
 }
 
-// TryGet 0未完成 1完成 -1超时
+func (p Promise[T]) Cancel() bool {
+	if p.status.Load() == PromisePending {
+		p.status.Store(PromiseCanceled)
+		p.waiter.Done()
+		return true
+	}
+	return false
+}
+
 func (p Promise[T]) TryGet() PromiseRes[T] {
-	return PromiseRes[T]{int8(p.pending.Load()), p.result}
+	return PromiseRes[T]{int8(p.status.Load()), p.result}
 }
 
-func Promise_[T any](timeout time.Duration) (Promise[T], func(T)) {
+func Promise_[T any]() (Promise[T], func(T)) {
 	p := Promise[T]{&_PromisePin[T]{}}
 	p.waiter.Add(1)
 	f := func(t T) {
-		if p.pending.Load() == PromisePending {
-			p.pending.Store(PromiseComplete)
+		if p.status.Load() == PromisePending {
+			p.status.Store(PromiseCompleted)
 			p.result = t
 			p.waiter.Done()
 		}
-	}
-	if timeout > 0 {
-		time.AfterFunc(timeout, func() {
-			if p.pending.Load() == PromisePending {
-				p.pending.Store(PromiseTimeout)
-				p.waiter.Done()
-			}
-		})
 	}
 	return p, f
 }
