4fcb5e4446d97fdf1963eae6144ad4defe793bbd
lee <876794356@qq.com>
2025-09-18 22:30:25 +0800

    Update cast

--- ext/cast.go
+++ ext/cast.go
@@ -5,22 +5,22 @@ import (
 	"unsafe"
 )
 
-// UnsafeCast 对值类型做二进制重新解释 谨慎使用
+// UnsafeCast Be cautious when performing binary reinterpretation on value types.
 func UnsafeCast[R, T any](t T) R {
 	return *(*R)(unsafe.Pointer(&t))
 }
 
-// BytesCastStr 为了减少内存copy 使用的不安全转换 谨慎使用 传入的bytes不允许再做修改
+// BytesCastStr Avoid unsafe conversions that cause memory copies.
 func BytesCastStr(bytes []byte) string {
 	return unsafe.String(unsafe.SliceData(bytes), len(bytes))
 }
 
-// StrCastBytes 为了减少内存copy 使用的不安全转换 谨慎使用
+// StrCastBytes Avoid unsafe conversions that cause memory copies.
 func StrCastBytes(str string) []byte {
-	return unsafe.Slice(unsafe.StringData(string(str)), len(str))
+	return unsafe.Slice(unsafe.StringData(str), len(str))
 }
 
-// BytesCastNumber 二进制转数字
+// BytesCastNumber Big-endian conversion
 func BytesCastNumber[T Number](bytes []byte) T {
 	t := *new(T)
 	size := unsafe.Sizeof(t)
@@ -37,7 +37,7 @@ func BytesCastNumber[T Number](bytes []byte) T {
 	return t
 }
 
-// BytesCastNumberLe 二进制转数字 小端
+// BytesCastNumberLe Little-endian conversion
 func BytesCastNumberLe[T Number](bytes []byte) T {
 	t := *new(T)
 	size := unsafe.Sizeof(t)
