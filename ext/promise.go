ed59311ecc381363f002ea1296446197ee638603
lee <876794356@qq.com>
2026-09-09 16:13:02 +0800

    update promise

--- ext/promise.go
+++ ext/promise.go
@@ -10,7 +10,6 @@ const (
 	_PromisePending    = 0
 	_PromiseCompleting = 1
 	_PromiseCompleted  = 2
-	_PromiseCanceled   = -1
 )
 
 type Promise[T any] struct {
@@ -27,33 +26,8 @@ func (p Promise[T]) Pending() bool {
 	return p.status.Load() == _PromisePending
 }
 
-func (p Promise[T]) Canceled() bool {
-	return p.status.Load() == _PromiseCanceled
-}
-
 func (p Promise[T]) Completed() bool {
 	return p.status.Load() == _PromiseCompleted
-}
-
-func (p Promise[T]) Done() bool {
-	s := p.status.Load()
-	return s == _PromiseCompleted || s == _PromiseCanceled
-}
-
-func (p Promise[T]) Timeout(d time.Duration) *time.Timer {
-	return time.AfterFunc(d, func() {
-		if p.status.CompareAndSwap(_PromisePending, _PromiseCanceled) {
-			p.waiter.Done()
-		}
-	})
-}
-
-func (p Promise[T]) Cancel() bool {
-	if p.status.CompareAndSwap(_PromisePending, _PromiseCanceled) {
-		p.waiter.Done()
-		return true
-	}
-	return false
 }
 
 func (p Promise[T]) Complete(v T) bool {
@@ -66,9 +40,25 @@ func (p Promise[T]) Complete(v T) bool {
 	return false
 }
 
-func (p Promise[T]) Await() Opt[T] {
+func (p Promise[T]) CompleteAfter(d time.Duration, fn func() T) *time.Timer {
+	if fn == nil {
+		panic("promise: nil completion function")
+	}
+	return time.AfterFunc(d, func() {
+		if p.status.CompareAndSwap(
+			_PromisePending,
+			_PromiseCompleting,
+		) {
+			p.result = fn()
+			p.status.Store(_PromiseCompleted)
+			p.waiter.Done()
+		}
+	})
+}
+
+func (p Promise[T]) Await() T {
 	p.waiter.Wait()
-	return p.TryGet()
+	return p.result
 }
 
 func (p Promise[T]) TryGet() Opt[T] {
@@ -83,30 +73,3 @@ func Promise_[T any]() Promise[T] {
 	p.waiter.Add(1)
 	return p
 }
-
-//func (p Promise[T]) Cancel() bool {
-//	if p.status.Load() == _PromisePending {
-//		p.locker.Lock()
-//		defer p.locker.Unlock()
-//		if p.status.Load() == _PromisePending {
-//			defer p.waiter.Done()
-//			p.status.Store(_PromiseCanceled)
-//			return true
-//		}
-//	}
-//	return false
-//}
-
-//func (p Promise[T]) Complete(t T) bool {
-//	if p.status.Load() == _PromisePending {
-//		p.locker.Lock()
-//		defer p.locker.Unlock()
-//		if p.status.Load() == _PromisePending {
-//			defer p.waiter.Done()
-//			p.result = t
-//			p.status.Store(_PromiseCompleted)
-//			return true
-//		}
-//	}
-//	return false
-//}
